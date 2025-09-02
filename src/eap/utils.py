36b06a95e6725b93687bd8d20f0b2f914680966a
Nura J <nura.aljaafari@postgrad.manchester.ac.uk>
2025-09-02 11:28:46 +0100

    revert tokenize_plus to original state

--- src/eap/utils.py
+++ src/eap/utils.py
@@ -12,8 +12,33 @@ from einops import einsum
 
 from .graph import Graph, AttentionNode, LogitNode
 
-
 def tokenize_plus(model: HookedTransformer, inputs: List[str], max_length: Optional[int] = None):
+    """
+    Tokenizes the input strings using the provided model.
+
+    Args:
+        model (HookedTransformer): The model used for tokenization.
+        inputs (List[str]): The list of input strings to be tokenized.
+
+    Returns:
+        tuple: A tuple containing the following elements:
+            - tokens (torch.Tensor): The tokenized inputs.
+            - attention_mask (torch.Tensor): The attention mask for the tokenized inputs.
+            - input_lengths (torch.Tensor): The lengths of the tokenized inputs.
+            - n_pos (int): The maximum sequence length of the tokenized inputs.
+    """
+    if max_length is not None:
+        old_n_ctx = model.cfg.n_ctx
+        model.cfg.n_ctx = max_length
+    tokens = model.to_tokens(inputs, prepend_bos=True, padding_side='right', truncate=(max_length is not None))
+    if max_length is not None:
+        model.cfg.n_ctx = old_n_ctx
+    attention_mask = get_attention_mask(model.tokenizer, tokens, True)
+    input_lengths = attention_mask.sum(1)
+    n_pos = attention_mask.size(1)
+    return tokens, attention_mask, input_lengths, n_pos
+
+def tokenize_plus1(model: HookedTransformer, inputs: List[str], max_length: Optional[int] = None):
     """
     Tokenizes the input strings using the provided model.
 
